b4b2ca47c93583b3dd4964e71fe8539bacf4f5ee
Tijana Milošević <tm20160111@student.fon.bg.ac.rs>
2020-11-24 23:16:28 +0100

    model update

--- src/netflix_statistics/popularity_prediction.clj
+++ src/netflix_statistics/popularity_prediction.clj
@@ -10,20 +10,57 @@
 
 ;; movie popularity score is 10 points:
 ;; 1) actors popularity - 0-not popular, 1-mid-popular, 2-popular
-;; 2) streaming platform popularity-  0-not popular, 1-mid-popular, 2-popular
-;; 3) director popularity-  0-not popular, 1-mid-popular, 2-popular
+;; 2) streaming platform popularity-  0-not popular, 1-mid-popular, 2-popular, 3-very popular
+;; 3) director popularity-  0-not popular, 1-popular
 ;; 4) IMDb rating-  0-not popular, 1-mid-popular, 2-popular
 ;; 5) Rotten Tomatoes rating-  0-not popular, 1-mid-popular, 2-popular
 
 ;; RANKING LIST:
-;; 0,1,2,3,4,5 - not popular movies
-;; 6,7,8,9,10 - popular movies
+;; 0,1,2,3,4,5 points - not popular movies
+;; 6,7,8,9,10 points- popular movies
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; IMPLEMENTATION ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; load movies
+(def json-movies (slurp "resources/movies.json"))
+(def movies (json/read-str json-movies :key-fn keyword))
+
+
 
 ;; actors points:
 
+; load json file
+(def json-actors (slurp "resources/actors.json"))
+(def actors (json/read-str json-actors :key-fn keyword))
+
+
+
+
+
 
 ;; streaming platform popularity:
-;; acording to statista.com most popular streaming platform are Netflix and Hulu(2 points), Prime video (1 point) and Disney (0 points)
+;; acording to statista.com most popular streaming platform are:
+;; Netflix( 3 points), 
+;; Hulu(2 points), 
+;; Prime video (1 point)
+;; and Disney (0 points).
+
+
+
+
+
+;; director popularity:
+;; here is list 100 most popular directors in the world, so is movies director is on this list its 1 point, if not 0 poins
+
+; load json file
+(def json-directors (slurp "resources/directors.json"))
+(def directors (json/read-str json-directors :key-fn keyword))
+
+
+
+
+
+
 
 
 ;; IMDb rating:
@@ -33,10 +70,17 @@
 ;;  and third-group-rating 2 points.
 
 
+
+
+
+
+
 ;;Rotten Tomatoes rating:
 ;; according to Rotten Tomatoes web site:
 ;; >60% - 2 points
 ;; 30% - 59% - 1 point
 ;; 0% - 29% - 0 points
+
+
 
 
