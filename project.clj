ca78067722d500dbc1619fc86d9f23f9656f0f43
Tijana Milošević <tm20160111@student.fon.bg.ac.rs>
2020-12-20 16:52:32 +0100

    project version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject netflix_statistics "0.1.0-SNAPSHOT"
+(defproject netflix_statistics "0.1.1"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "1.0.0"]
                  [org.clojure/data.csv "1.0.0"]
