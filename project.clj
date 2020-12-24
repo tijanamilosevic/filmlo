07a471517defa310ced70a94c5b086ebbc2bfbbe
Tijana Milošević <tm20160111@student.fon.bg.ac.rs>
2020-12-24 23:13:07 +0100

    v0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject filmlo "0.1.1"
+(defproject filmlo "0.1.2"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "1.0.0"]
                  [org.clojure/data.csv "1.0.0"]
